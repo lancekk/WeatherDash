b527218f1a573755c5e7fcb45cbccfb83f32a5d4
Lance Kelley <lancekkcodes@gmail.com>
2021-09-18 15:35:47 -0500

    call weather endpoint with city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
 const apiKey = "d1ed7be948d11ed8ac35d1aa806592ad";
 const apiUrl = 'https://api.openweathermap.org/data/2.5/';
 const onecall = 'onecall?';
+const weather = 'weather?';
+const currentWeatherDiv = document.querySelector('#current-weather');
 
 let lastRes = null;
+const displayCurrentWeather = function(cityName, date, temp, wind, humidity, uv) {
+  ;
+}
+
 const weatherCall = function(cityName) {
-  fetch(apiUrl + onecall +
+  return fetch(apiUrl + weather +
     [
       `q=${cityName}`,
-      `exclude=${['minutely', 'hourly', 'alerts'].join(',')}`,
       `appid=${apiKey}`
     ].join('&'))
-    .then(res => {
-      res.json();
-    }).then(json => {
-      console.log(json);
-      lastRes = json;
+    .then(res => res.json())
+    .then(o => {
+      lastRes = o;
+      console.log(o);
     })
 }
